6fa442081ba47f592d5967d78329d2aa82d52c3c
timhagn <mail@timhagn.com>
2018-12-06 22:22:18 +0100

    update dist

--- dist/sw.js
+++ dist/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-71235146bbc460931918.js"
+    "url": "webpack-runtime-22d3dd037bb60084dc59.js"
   },
   {
     "url": "app-2767a2f7366a3ce05dfe.js"
@@ -36,13 +36,13 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0ae66ff2b2493b998caaf64c16e801fb"
+    "revision": "6c4a5a1fd6b4dabc75824eabdefca539"
   },
   {
     "url": "0.455451cccc697880d62b.css"
   },
   {
-    "url": "1-40b0381bbbe38cf53460.js"
+    "url": "1-181312e8fde5ed86e91a.js"
   },
   {
     "url": "component---src-pages-404-js-bec3112525ce98385195.js"
